60d3a8c0e92d86c767ab9fb99ab882c58a497e11
Marcin Janiczek <65963842+Marcin399@users.noreply.github.com>
2025-05-10 14:35:26 +0200

    Initial Commit

---
+++ PaymentDemoTests/PaymentDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentDemoTests.swift
+//  PaymentDemoTests
+//
+//  Created by Marcin Janiczek on 10/05/2025.
+//
+
+import Testing
+@testable import PaymentDemo
+
+struct PaymentDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
